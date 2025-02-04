e636cc3b9e40e89f7e9dcc98251a4e409ce0f840
Muthu K <muthuvk@hotmail.com>
2025-02-04 16:26:04 +1100

    Tailwind not working but setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex intems-center justify-center bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold"> Trading Journal App </h1>
+    </div>
+  );
+}
+
+export default App;
